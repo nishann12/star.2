970eda905f37c368c38f10ab55cee131aa391880
nishan <nishanmohammed0097@gmail.com>
2024-10-24 15:42:41 +0530

    first

---
+++ script.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+let count=0
+const prompt = msg => {
+    fs.writeSync(1, String(msg));
+    let s = '', buf = Buffer.alloc(1),i=0,numberOfNewLine=0,stdinBuffer = Buffer.alloc(1),inc=0;
+    buf.fill(0);
+    while ((buf[0] - 10 && buf[0] - 13)) {
+       
+        s += buf, fs.readSync(0, buf, 0, 1, 0);
+        if(buf[0] - 10===0 || buf[0] - 13===0){
+            i++;
+            // console.log('first',i)
+        }
+    }
+    fs.readSync(0,stdinBuffer,0,1,null);
+    count++;
+    return s.slice(1).replace(/^\n/, '');
+};
+// const result = prompt('Input something: ');
+// const result2 = prompt('Input something: ');
+// const result3 = prompt('Input something: ');
+// console.log('Your input was: ' + result);
+// console.log('Your input was: ' + result2);
+// // console.log('Your input was: ' + result3);
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the number of stars in  line: ', (stars) => {
+  rl.question('Enter the number of lines: ', (lines) => {
+    for (let i = 0; i < lines; i++) {
+      console.log('*'.repeat(stars));
+    }
+    rl.close();
+  });
+});
